3cf8c3cb8e3d2ff043593202a6a73945783c2615
MelicAjlan <ajlanuidesigner@gmail.com>
2023-02-01 23:39:21 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HomeScreen from './HomeScreen.js';
+import './Nav.js';
+
+function App() {
+  return (
+    <div className="App">
+      <HomeScreen />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyArLZyyzt6BlGNFIgqOvdQo026iPtgWxWg",
+    authDomain: "netflix-clone-ajlan.firebaseapp.com",
+    projectId: "netflix-clone-ajlan",
+    storageBucket: "netflix-clone-ajlan.appspot.com",
+    messagingSenderId: "262887079810",
+    appId: "1:262887079810:web:a4dec4aad49e999864999b",
+    measurementId: "G-JXQDM95NML"
+  };

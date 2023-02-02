5ff532bb42fc620e93464926efdc264f84888175
MelicAjlan <ajlanuidesigner@gmail.com>
2023-02-02 15:14:56 +0100

    Initial Commit

--- src/Axios.js
+++ src/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "https://api.themoviedb.org/3",
+    baseURL: "https://api.themoviedb.org/3/movie/550?api_key=4e42554dc9ad5bc696bae4c92719bc92",
 });
 
 export default instance;
--- src/Banner.js
+++ src/Banner.js
@@ -1,42 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import './Banner.css';
+import axios from "./Axios";
+import requests from "./Requests";
 
 function Banner() {
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                Math.floor(Math.random() * request.data.results.lenght - 1)
+                ]
+            )
+            return request;
+        }
+
+        fetchData();
+    }, []);
+
+    console.log(movie);
+
 
     function truncate(string, n) {
-        return string.lenght > n ? string.substr(0, n - 1) + '...' : string;
+        return string?.lenght > n ? string.substr(0, n - 1) + "..." : string;
     }
 
-  return (
-    <header className="banner" 
-    style={{
-        backgroundSize: "cover",
-        backgroundImage: `url("https://raw.githubusercontent.com/thatanjan/netflix-clone-yt/youtube/media//banner.jpg")`,
-        backgroundPosition: "center center",
-    }}>
+    return (
+        <header
+            className="banner"
+            style={{
+                backgroundSize: "cover",
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg${movie}")`,
+                backgroundPosition: "center center",
+            }}>
 
-        <div className="banner__contents">
-            <h1 className="banner__title">Movie Name</h1>
-            <div className="banner__buttons">
-                <button className="banner__button">Play</button>
-                <button className="banner__button">My List</button>
+            <div className="banner__contents">
+                <h1 className="banner__title">Movie Name</h1>
+                <div className="banner__buttons">
+                    <button className="banner__button">Play</button>
+                    <button className="banner__button">My List</button>
+                </div>
+                <h1 className="banner__description">
+                    {truncate(
+                        `This is a test description This is a test description
+                    This is a test descriptionThis is a test description
+                    This is a test descriptionThis is a test description
+                    This is a test descriptionThis is a test description
+                    This is a test descriptionThis is a test description 
+                    This is a test descriptionThis is a test description 
+                    This is a test descriptionThis is a test description 
+                    This is a test descriptionThis is a test description`,
+                        100
+                    )}
+                </h1>
             </div>
-            <h1 className="banner__description">
-                {truncate(
-                    `This is a test description This is a test description
-                    This is a test descriptionThis is a test description
-                    This is a test descriptionThis is a test description
-                    This is a test descriptionThis is a test description
-                    This is a test descriptionThis is a test description`, 
-                    150
-                )}
-            </h1>
-        </div>
 
-        <div className="banner--fadeButton" />
+            <div className="banner--fadeButton" />
 
-    </header>
-  )
+        </header>
+    )
 }
 
 export default Banner
